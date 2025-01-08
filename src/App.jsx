e1dafea019ac153fd0f1ef422defe0b5b235f39f
Avishek Mondal <avizaz040@gmail.com>
2025-01-08 12:22:42 +0530

    update home path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./Pages/Home";
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "/Blog",
       element: (
         <div>
           <Navbar />,
