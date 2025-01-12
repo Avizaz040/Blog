0d2d7c3cfbd5b3b0694cd8db0149cd25bb9d2e50
Avishek Mondal <avizaz040@gmail.com>
2025-01-13 00:59:47 +0530

    url update 4

--- src/Pages/LogoutPage.jsx
+++ src/Pages/LogoutPage.jsx
@@ -5,7 +5,7 @@ function LogoutPage() {
   return (
     <div className='flex flex-col justify-center items-center h-screen gap-4'>
       <h1 className='text-5xl text-red-800 font-semibold'>You are successfully logged out</h1>
-      <p className='text-3xl'>To login again please click <Link to="/Blog/login" className='text-blue-600 font-semibold underline'>here</Link> </p>
+      <p className='text-3xl'>To login again please click <Link to="/login" className='text-blue-600 font-semibold underline'>here</Link> </p>
       
     </div>
   )
