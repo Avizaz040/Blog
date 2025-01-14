e040d00732a78e359fad98d3bc9f00c993bf0197
Avishek Mondal <avizaz040@gmail.com>
2025-01-14 13:56:59 +0530

    responsive update 6

--- src/Components/NewsletterReminder.jsx
+++ src/Components/NewsletterReminder.jsx
@@ -10,11 +10,11 @@ function NewsletterReminder() {
             Subscribe today and get exclusive access to premium content!
           </p>
         </div>
-        <div className="h-[45px] w-[280px] lg:h-[66px] lg:w-[570px] border-[1px] border-[#1067CB] flex flex-col lg:flex-row justify-between  py-[8px] lg:py-[6px] px-4 lg:pl-[28px] pr-[16px] lg:pr-[6px] rounded-[19px] gap-6 lg:gap-2">
+        <div className="h-[38px] w-full lg:h-[78px] lg:w-[778px] border-[1px] border-[#1067CB] flex flex-col items-center lg:flex-row justify-between  py-[6px] lg:pl-[28px] lg:pr-[6px] rounded-[19px] gap-4 lg:gap-2">
           <input
             type="text"
-            placeholder="Enter your E-mail to subscribe"
-            className="w-full lg:w-2/3 font-[Inter] font-[400] outline-none placeholder:text-[16px]"
+            placeholder="Enter your e-mail to subscribe"
+            className="w-4/5 lg:w-2/3 font-[Inter] font-[400] outline-none placeholder:text-[16px] text-center"
           />
           <button className="py-[10px] px-[42px] lg:py-[18px] lg:px-[54px] rounded-[13px] bg-[#1067CB] text-white font-[Inter] text-[16px] lg:text-[20px] lg:leading-[18px]">
             Subscribe
--- src/Components/SubscribeSection.jsx
+++ src/Components/SubscribeSection.jsx
@@ -16,11 +16,11 @@ function SubscribeSection() {
           Subscribe today and get exclusive access to premium content!
         </p>
 
-        <div className="h-[38px] w-[280px] lg:h-[78px] lg:w-[778px] border-[1px] border-[#1067CB] flex flex-col items-center lg:flex-row justify-between  py-[6px] lg:pl-[28px] pr-[6px] rounded-[19px] gap-4 lg:gap-2">
+        <div className="h-[38px] w-full lg:h-[78px] lg:w-[778px] border-[1px] border-[#1067CB] flex flex-col items-center lg:flex-row justify-between  py-[6px] lg:pl-[28px] lg:pr-[6px] rounded-[19px] gap-4 lg:gap-2">
           <input
             type="text"
-            placeholder="Enter your E-mail to subscribe"
-            className="w-2/3 font-[Inter] font-[400] outline-none placeholder:text-[16px]"
+            placeholder="Enter your e-mail to subscribe"
+            className="w-4/5 lg:w-2/3 font-[Inter] font-[400] outline-none placeholder:text-[16px] text-center"
           />
           <button className="py-[10px] px-[42px] lg:py-[18px] lg:px-[54px] rounded-[13px] bg-[#1067CB] text-white font-[Inter] text-[16px] lg:text-[24px] leading-[29.05px]">
             Subscribe
