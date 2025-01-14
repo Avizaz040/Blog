33386295ccc39724887b9abae02d2b5b335ae74f
Avishek Mondal <avizaz040@gmail.com>
2025-01-14 14:13:50 +0530

    responsive update 7

--- src/Pages/LogoutPage.jsx
+++ src/Pages/LogoutPage.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function LogoutPage() {
   return (
-    <div className='flex flex-col justify-center items-center h-screen gap-4'>
-      <h1 className='text-5xl text-red-800 font-semibold'>You are successfully logged out</h1>
-      <p className='text-3xl'>To login again please click <Link to="/login" className='text-blue-600 font-semibold underline'>here</Link> </p>
+    <div className='flex flex-col justify-center items-center text-center h-screen gap-4'>
+      <h1 className=' text-2xl lg:text-5xl text-red-800 font-semibold'>You are successfully logged out</h1>
+      <p className='text-xl lg:text-3xl'>To login again please click <Link to="/login" className='text-blue-600 font-semibold underline'>here</Link> </p>
       
     </div>
   )
