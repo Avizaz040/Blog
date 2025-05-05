c40189d5b23d29cd0a3b30189715e7c86c129380
Avishek Mondal <103316401+Avizaz040@users.noreply.github.com>
2025-05-05 20:33:18 +0530

    Update LoginPage.jsx

--- Frontend/src/Pages/LoginPage.jsx
+++ Frontend/src/Pages/LoginPage.jsx
@@ -18,7 +18,7 @@ function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://blog-5ipy.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
